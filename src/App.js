40d00860c028c212aaab1f66fae606717f95b149
rkdalswl718 <rkdalswl070718@gmail.com>
2024-07-17 22:30:49 +0900

    add 페이지 전환시 애니메이션 추가

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Randing from './pages/randing/index';
-import Start from './pages/start/index'
+import Start from './pages/start/index';
+import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Randing />} />
-        <Route path='/start' element={<Start />} />
-      </Routes>
+      <AnimatedRoutes />
     </BrowserRouter>
+  );
+}
+
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <TransitionGroup>
+      <CSSTransition key={location.key} classNames="fade" timeout={300}>
+        <Routes location={location}>
+          <Route path="/" element={<Randing />} />
+          <Route path="/start" element={<Start />} />
+        </Routes>
+      </CSSTransition>
+    </TransitionGroup>
   );
 }
 
--- src/pages/randing/index.jsx
+++ src/pages/randing/index.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import Logo from '../../assets/Logo';
-import './style.css';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Logo from "../../assets/Logo";
+import "./style.css";
 
-const Rading = () => {
+const Randing = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/start');
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
   return (
     <div className="container">
-      <Logo className="logo"/>
+      <Logo className="logo" />
     </div>
   );
 };
 
-export default Rading;
+export default Randing;
