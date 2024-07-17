389d06ca88ca8721770c616265ccf0fd78ff6630
kangsiwoo <2023003@bssm.hs.kr>
2024-07-18 04:45:31 +0900

    feat: mypage

--- src/pages/MyPage/index.jsx
+++ src/pages/MyPage/index.jsx
@@ -5,6 +5,9 @@ import {DefaultProfile, MyDatasLine} from "./style";
 
 const MyPage = () => {
     const userName = "강민지"
+    const serialCode = "ABCDE"
+    const userID = "rkdalswl718"
+
     return (
         <s.Container>
             <s.Header>
@@ -15,11 +18,14 @@ const MyPage = () => {
             <s.UserName>{userName}님</s.UserName>
             <s.MyData><s.MyDataText>내 정보</s.MyDataText></s.MyData>
             <s.MyDatas>
-                <s.MyDatasText>시리얼 코드</s.MyDatasText>
-                {/*<s.MyDatasLine />*/}
-                <s.MyDatasText>시리얼 코드</s.MyDatasText>
+                <s.MyDatasCenterBox>
+                    <s.MyDatasText>시리얼 코드: {serialCode}</s.MyDatasText>
+                    <s.MyDatasLine/>
+                    <s.MyDatasText>아이디: {userID}</s.MyDatasText>
+                </s.MyDatasCenterBox>
             </s.MyDatas>
-            <Footer />
+            <s.StyledButton>로그아웃</s.StyledButton>
+            <Footer/>
         </s.Container>
     )
 }
--- src/pages/MyPage/style.js
+++ src/pages/MyPage/style.js
@@ -1,21 +1,20 @@
 import styled, { css } from 'styled-components';
 import Logo from "../../assets/Logo";
-import DefaultImage from "../../assets/mypage/defaultProfile.png"
-import {text} from "../start/style";
+import DefaultImage from "../../assets/mypage/defaultProfile.png";
+import { text } from "../start/style";
 
 export const Container = styled.div`
     position: relative;
-    
     display: flex;
     flex-direction: column;
     min-height: 100vh;
     background-color: #F9F9F9;
-`
+`;
 
 export const stLogo = styled(Logo)`
     width: 25.06px;
     height: 23.53px;
-`
+`;
 
 export const Header = styled.header`
     position: relative;
@@ -23,8 +22,7 @@ export const Header = styled.header`
     left: 25.75px;
     width: 80.25px;
     height: 23.53px;
-    //background-color: #2f71d3;
-`
+`;
 
 export const LogoText = styled.p`
     position: absolute;
@@ -34,32 +32,26 @@ export const LogoText = styled.p`
     color: #5A51FF;
     font-size: 17px;
     font-family: Pretendard-Regular;
-`
+`;
 
 export const DefaultProfile = styled.div`
     width: 80px;
     height: 80px;
-    
     position: relative;
-    
     top: 98px;
     left: 50%;
     transform: translateX(-50%);
-    
     background-image: url(${DefaultImage});
-`
+`;
 
 export const UserName = styled.p`
     width: 100vw;
     height: 80px;
-    
     text-align: center;
     font-family: Pretendard-Regular;
-
     position: relative;
-
     top: 113px;
-`
+`;
 
 export const MyData = styled.div`
     font-family: Pretendard-Regular;
@@ -67,47 +59,80 @@ export const MyData = styled.div`
     left: 50%;
     top: 283px;
     transform: translateX(-50%);
-    
     background-color: #FFF;
     border-radius: 10px;
-    
     width: 320px;
     height: 40px;
-`
+`;
 
 export const MyDataText = styled.p`
     position: absolute;
     top: 50%;
     transform: translateY(-50%);
     left: 16px;
-`
+`;
 
 export const MyDatas = styled.div`
     position: absolute;
     top: 330px;
     left: 50%;
     transform: translateX(-50%);
-    
     background-color: #FFF;
     border-radius: 10px;
-    
     width: 320px;
     height: 94px;
-`
+`;
 
 export const MyDatasText = styled.p`
     position: relative;
     font-family: Pretendard-Regular;
     padding: 10px 0;
     left: 16px;
-    
     color: #7A7A7A7A;
-`
+`;
 
-export const MyDatasLine = styled.br`
+export const MyDatasLine = styled.div`
     position: relative;
     left: 16px;
     width: 288px;
-    height: 1px;
+    height: 0;
     border: 1px solid #F3F3F3F3;
-`
+`;
+
+export const MyDatasCenterBox = styled.div`
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+`;
+
+export const InputContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 20px;
+    width: 100%;
+    max-width: 375px;
+`;
+
+export const StyledButton = styled.button`
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+
+    bottom: 89px;
+
+    width: 320px;
+    max-width: 375px;
+    color: black;
+    border: none;
+    font-size: 16px;
+    font-weight: 600;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 10px 130px;
+    height: 48px;
+    background: #ffffff;
+    border-radius: 10px;
+    font-family: 'pretendard';
+`;
