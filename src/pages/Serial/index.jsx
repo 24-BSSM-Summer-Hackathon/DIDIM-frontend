cc436d84dce729407d509b488fe1632bd3479714
rkdalswl718 <rkdalswl070718@gmail.com>
2024-07-18 02:23:54 +0900

    add serial code page 기능 완성

--- src/pages/Serial/index.jsx
+++ src/pages/Serial/index.jsx
@@ -1,24 +1,59 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import * as s from "./style";
 import Button from '../../components/Button';
 
 const Serial = () => {
   const serialInputs = Array(5).fill("");
+  const [inputFocused, setInputFocused] = useState(false);
+  const inputRefs = useRef([]);
+  const [keyboardHeight, setKeyboardHeight] = useState(0);
 
-  
+  useEffect(() => {
+    const handleResize = () => {
+      const newKeyboardHeight = window.innerHeight < window.screen.height ? window.screen.height - window.innerHeight : 0;
+      setKeyboardHeight(newKeyboardHeight);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const handleInputFocus = () => {
+    setInputFocused(true);
+  };
+
+  const handleInputBlur = () => {
+    setInputFocused(false);
+  };
+
+  const handleInputChange = (index, e) => {
+    if (e.target.value.length === 1 && index < serialInputs.length - 1) {
+      inputRefs.current[index + 1].focus();
+    }
+  };
 
   return (
     <s.SerialContainer>
       <s.TextContainer>
-      <s.Logo />
+        <s.Logo />
         <s.MainText>링에 적힌 시리얼코드를<br/>입력하세요!</s.MainText>
       </s.TextContainer>
       <s.InputContainer>
         {serialInputs.map((_, index) => (
-          <s.SerialInput key={index} />
+          <s.SerialInput
+            key={index}
+            ref={el => inputRefs.current[index] = el}
+            onFocus={handleInputFocus}
+            onBlur={handleInputBlur}
+            onChange={(e) => handleInputChange(index, e)}
+            maxLength={1}
+          />
         ))}
       </s.InputContainer>
+      <s.Spacer inputFocused={inputFocused} keyboardHeight={keyboardHeight} />
+      <s.ButtonWrapper inputFocused={inputFocused} keyboardHeight={keyboardHeight}>
         <Button text="등록"></Button>
+      </s.ButtonWrapper>
     </s.SerialContainer>
   );
 };
--- src/pages/Serial/style.js
+++ src/pages/Serial/style.js
@@ -1,11 +1,11 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import LogoImage from '../../assets/Logo';
 
-export const SerialLayout = styled.div ` 
-font-family: 'pretendard';
-height: 100vh;
-width: 100vw;
-`
+export const SerialLayout = styled.div`
+  font-family: 'pretendard';
+  height: 100vh;
+  width: 100vw;
+`;
 
 export const SerialContainer = styled.div`
   display: flex;
@@ -43,13 +43,14 @@ export const InputContainer = styled.div`
 `;
 
 export const SerialInput = styled.input`
-width: 56px;
-height: 60px;
+  width: 56px;
+  height: 60px;
   background: #FFFFFF;
   border: 1px solid #f2f2f2;
   border-radius: 10px;
   text-align: center;
-  font-size: 18px;
+  font-size: 24px;
+  font-weight: 700;
   outline: none;
 
   &:focus {
@@ -57,3 +58,26 @@ height: 60px;
   }
 `;
 
+export const Spacer = styled.div`
+  flex-grow: 1;
+  ${({ inputFocused, keyboardHeight }) =>
+    inputFocused &&
+    css`
+      height: ${keyboardHeight}px;
+    `}
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  bottom: 50px;
+  width: calc(100% - 48px);
+  transition: bottom 0.3s ease;
+  ${({ inputFocused, keyboardHeight }) =>
+    inputFocused &&
+    css`
+      bottom: ${keyboardHeight + 50}px;
+    `}
+`;
