8fd991b182ad8df3e426a94c4256fc5d9e5003b7
rkdalswl718 <rkdalswl070718@gmail.com>
2024-07-17 22:27:33 +0900

    add startpage완성

--- src/pages/start/index.jsx
+++ src/pages/start/index.jsx
@@ -5,7 +5,16 @@ const Start = () => {
   return (
     <s.StartBackground>
       <s.StrartLayout>
-        <s.StyledLogo />
+        <s.TextContainer>
+        <s.StyledLogo color="#FFFFFF" />
+        <s.text>
+          <p>지금 바로 시작하세요.<br/>안전한 하루의 시작, 디딤 </p>
+        </s.text>
+        </s.TextContainer>
+        <s.ButtonContainer>
+        <s.LoginButton>로그인</s.LoginButton>
+        <s.ButtonInfo>이미 계정이 있으신가요?</s.ButtonInfo>
+        </s.ButtonContainer>
       </s.StrartLayout>
     </s.StartBackground>
   );
--- src/pages/start/style.js
+++ src/pages/start/style.js
@@ -15,10 +15,60 @@ export const StartBackground = styled.div`
 
 export const StrartLayout = styled.div`
   padding: 0 24px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
 `;
 
 export const StyledLogo = styled(Logo)`
-  width: 200px;
-  height: 200px;
+  width: 70px;
+  height: 70px;
   fill: ${({ color }) => color || "#FFFFFF"};
+`;
+
+export const text = styled.div`
+  color: #fff;
+  font-size: 24px;
+  font-weight: 600;
+  text-align: left;
+`;
+
+export const TextContainer = styled.div`
+  margin-top:100px ;
+  align-items: left;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: auto;
+  margin-bottom: 20px;
+`;
+
+export const LoginButton = styled.button`
+  justify-content: center;
+  align-items: center;
+  padding: 10px 135px;
+  border: none;
+  width: 320px;
+  height: 48px;
+  font-size: 16px;
+  font-weight: 700;
+  background: #FFFFFF;
+  color: #5A51FF;
+  border-radius: 10px;
+  margin-bottom: 8px;
+`;
+
+export const ButtonInfo = styled.div`
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 14px;
+  text-align: center;
+  color: #FFFFFF;
+  flex-grow: 0;
 `;
